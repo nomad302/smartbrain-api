3772840c06014695525679d5c9464f9ad6919900
nomad302 <sagar.nargundi@gmail.com>
2019-11-15 07:27:12 +0530

    node module updated

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "john",
+      email: "john@gmail.com",
+      password: "john123"
+    }
+  ]
+};
 app.get("/", (req, res) => {
   res.send("this is working");
 });
 
 app.post("/signin", (req, res) => {
-  res.send("signIn");
+  
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("app is running on port 3000");
 });
