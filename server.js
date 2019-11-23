574f522f2d88eb0bf8470167ca474369233c7f8b
nomad302 <sagar.nargundi@gmail.com>
2019-11-24 00:51:32 +0530

    knex added for database connect

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+const knex = require("knex");
+
+const db = knex({
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: "dev",
+    password: "bombay3333",
+    database: "smartbrain"
+  }
+});
+
+db.select("*")
+  .from("users")
+  .then(data => {
+    console.log(data);
+  });
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -20,9 +37,7 @@ app.get("/", (req, res) => {
   res.send("this is working");
 });
 
-app.post("/signin", (req, res) => {
-  
-});
+app.post("/signin", (req, res) => {});
 
 app.listen(3001, () => {
   console.log("app is running on port 3000");
